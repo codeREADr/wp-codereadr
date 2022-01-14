055c23c00e140e423a44140fe81555c9a737d470
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-01-14 21:23:50 +0200

    Adding admin settings page and some admin styles

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -61,6 +61,17 @@ class Admin {
 	 */
 	public function admin_hooks() {
 		add_action( 'admin_menu', array( $this, 'create_admin_menu_pages' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
+	}
+
+
+	/**
+	 * Enqueue admin enqueue_admin_styles
+	 *
+	 * @since 1.0.0
+	 */
+	function enqueue_admin_styles( $hook ) {
+		wp_enqueue_style( 'codereadr-admin-css', CODEREADR_PLUGIN_URL . 'includes/admin/assets/style.css', array(), '1.0' );
 	}
 
 	/**
@@ -73,11 +84,15 @@ class Admin {
 			__( 'Settings', 'codereadr' ),
 			__( 'CodeREADr', 'codereadr' ),
 			'manage_options',
-			'codereadr',
-			array( $this, 'render_settings_page' ),
+			'codereadr-settings',
+			null,
 			null,
 			30
 		);
+		add_submenu_page( 'codereadr-settings', __( 'CodeREADr', 'codereadr' ), __( 'Settings', 'codereadr' ), 'manage_options', 'codereadr-settings', array( $this, 'render_settings_page' ) );
+		add_submenu_page( 'codereadr-settings', __( 'Services', 'codereadr' ), __( 'Services', 'codereadr' ), 'manage_options', 'codereadr-services', array( $this, 'render_services_page' ) );
+		add_submenu_page( 'codereadr-settings', __( 'Actions', 'codereadr' ), __( 'Actions', 'codereadr' ), 'manage_options', 'codereadr-actions', array( $this, 'render_actions_page' ) );
+		add_submenu_page( 'codereadr-settings', __( 'Responses', 'codereadr' ), __( 'Responses', 'codereadr' ), 'manage_options', 'codereadr-responses', array( $this, 'render_responses_page' ) );
 	}
 
 
@@ -87,7 +102,42 @@ class Admin {
 	 * @since 1.0.0
 	 */
 	public function render_settings_page() {
-		echo '<h2>Settings</h2>';
+		ob_start();
+		require_once CODEREADR_PLUGIN_DIR . 'includes/admin/pages/settings.php';
+		echo ob_get_clean();
+	}
+
+	/**
+	 * Render services page
+	 *
+	 * @since 1.0.0
+	 */
+	public function render_services_page() {
+		ob_start();
+		require_once CODEREADR_PLUGIN_DIR . 'includes/admin/pages/services.php';
+		echo ob_get_clean();
+	}
+
+	/**
+	 * Render actions page
+	 *
+	 * @since 1.0.0
+	 */
+	public function render_actions_page() {
+		ob_start();
+		require_once CODEREADR_PLUGIN_DIR . 'includes/admin/pages/actions.php';
+		echo ob_get_clean();
+	}
+
+	/**
+	 * Render responses page
+	 *
+	 * @since 1.0.0
+	 */
+	public function render_responses_page() {
+		ob_start();
+		require_once CODEREADR_PLUGIN_DIR . 'includes/admin/pages/responses.php';
+		echo ob_get_clean();
 	}
 
 }
---
+++ includes/admin/pages/settings.php
@@ -0,0 +1,16 @@
+<div class="codereadr-admin-settings-page">
+	<h3> CodeREADr Settings </h3>
+	<div class="codereadr-admin-settings-page__content">
+		<div class="codereadr-admin-settings-page__api-key">
+			<h4>API KEY</h4>
+			<div class="codereadr-admin-settings-page__api-key-label">Enter your api key here</div>
+			<input type="text" class="codereadr-admin-settings-page__api-key-input" />
+			<div class="codereadr-admin-settings-page__buttons">
+				<a class="codereadr-button-secondary">Reset settings</a>
+				<a class="codereadr-button-primary">Save settings </a>
+
+			</div>
+
+		</div>
+	</div>
+</div>
