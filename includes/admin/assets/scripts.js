d8b2815ef0912d9605306eb2c8c2fc07146834a7
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-01-22 22:18:57 +0200

    Handling modal on responses page

--- includes/admin/assets/scripts.js
+++ includes/admin/assets/scripts.js
@@ -2,6 +2,11 @@ jQuery(document).ready(function($) {
     $('.codereadr-admin-settings-page__buttons .codereadr-button-secondary').on('click', function(){ 
         $('.codereadr-admin-settings-page__api-key-input').val("");
         $('.codereadr-admin-settings-page__api-key-input').removeAttr("disabled");    
-    
+    });
+    $('.codereadr-add-new-response').on('click', function(){ 
+        $(".codereadr-add-response-modal").addClass('show');
+    });
+    $('.codereadr-add-response-modal .codereadr-button-secondary').on('click', function(){
+        $(".codereadr-add-response-modal").removeClass('show');
     })
 })
