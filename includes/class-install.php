166a5ea4283189bb52550a62eb4509854de9725d
Mohamed Magdy <md.magdy.sa@gmail.com>
2022-01-22 16:51:23 +0200

    Fixing a bug in class-install.php file

--- includes/class-install.php
+++ includes/class-install.php
@@ -21,21 +21,21 @@ class Install {
 	 *
 	 * @since 1.0.0
 	 */
-public static function init() {
-	add_action( 'init', array( __CLASS__, 'check_version' ), 5 );
-}
+	public static function init() {
+		add_action( 'init', array( __CLASS__, 'check_version' ), 5 );
+	}
 
 	/**
 	 * Check Quill forms version and run the updater is required.
 	 *
 	 * This check is done on all requests and runs if the versions do not match.
 	 */
-public static function check_version() {
-	if ( version_compare( get_option( 'codereadr_version' ), CODEREADR_VERSION, '<' ) ) {
-		self::install();
-		do_action( 'codereadr_updated' );
+	public static function check_version() {
+		if ( version_compare( get_option( 'codereadr_version' ), CODEREADR_VERSION, '<' ) ) {
+			self::install();
+			do_action( 'codereadr_updated' );
+		}
 	}
-}
 
 	/**
 	 * Install CodeReadr
@@ -43,28 +43,28 @@ public static function check_version() {
 	 * @since 1.0.0
 	 * @static
 	 */
-public static function install() {
-	// Check if we are not already running this routine.
-	if ( 'yes' === get_transient( 'codereadr_installing' ) ) {
-		return;
+	public static function install() {
+		// Check if we are not already running this routine.
+		if ( 'yes' === get_transient( 'codereadr_installing' ) ) {
+			return;
+		}
+
+		// If we made it till here nothing is running yet, lets set the transient now.
+		set_transient( 'codereadr_installing', 'yes', MINUTE_IN_SECONDS * 10 );
+
+		self::create_tables();
+		self::update_codereadr_version();
+
+		delete_transient( 'codereadr_installing' );
+
 	}
-
-	// If we made it till here nothing is running yet, lets set the transient now.
-	set_transient( 'codereadr_installing', 'yes', MINUTE_IN_SECONDS * 10 );
-
-	self::create_tables();
-	self::update_codereadr_version();
-
-	delete_transient( 'codereadr_installing' );
-
-}
 
 	/**
 	 * Create DB Tables
 	 *
 	 * @since 1.0.0
 	 */
-public static function create_tables() {
+	public static function create_tables() {
 		global $wpdb;
 
 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
@@ -105,9 +105,7 @@ public static function create_tables() {
 				date_created datetime NOT NULL,
 				date_updated datetime,
 				PRIMARY KEY  (ID)
-			) $charset_collate;
-			
-
+			) $charset_collate;";
 
 		dbDelta( $sql );
 	}
